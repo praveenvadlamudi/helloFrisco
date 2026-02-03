58f760583594e48f53233b762d882c2cd67decca
praveenvadlamudi <67073733+praveenvadlamudi@users.noreply.github.com>
2026-02-03 04:18:29 -0600

    Update helloFirst2.js

--- helloFirst2.js
+++ helloFirst2.js
@@ -56,7 +56,7 @@ async function submitSelections() {
     <p><strong>State:</strong> ${state}</p>
     <p><strong>Company:</strong> ${company}</p>
 	  <p><strong>Call Type:</strong> ${callType}</p>
-    <p><strong>Claim Number:</strong> ${claimNumber}</p>
+    <p><strong>Afni Claim Number:</strong> ${claimNumber}</p>
     <p style="color:green;"><b>✔ Flow Variables Update Initiated</b></p>
   `;
 }
